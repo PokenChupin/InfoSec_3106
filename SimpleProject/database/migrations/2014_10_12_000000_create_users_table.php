609114f218a397066884dff21e247876772828cd
PokenChupin <jrcelso2911@gmail.com>
2021-10-02 22:18:50 +0800

    Add Token to User Table
    User Table

--- SimpleProject/database/migrations/2014_10_12_000000_create_users_table.php
+++ SimpleProject/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,14 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('api_token', 80)->after('password')
-                        ->unique()
-                        ->nullable()
-                        ->default(null);
+            
+            $table->string('name');       
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('token', 80)
+                        ->unique()
+                        ->nullable()
+                        ->default(null);
             $table->rememberToken();
             $table->timestamps();
         });
